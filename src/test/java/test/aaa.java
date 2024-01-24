4328cf8b2d178e3dc2557f72c40fc850eb478d31
sapphirus <sapphirus@DESKTOP-H3BL2K3>
2024-01-24 15:25:35 +0530

    srinivas

--- src/test/java/test/aaa.java
+++ src/test/java/test/aaa.java
@@ -1,8 +1,28 @@
 package test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class aaa
 {
-	public static void main(String[] args) {
-		System.out.println("Srinias Teegala");
+	@Test
+	public void tests() throws Exception
+	{
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://lms-sis-demo.sapphirus.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@formcontrolname='uname']")).sendKeys("srinivas.test");
+		driver.findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys("Welcome1!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()=' Sign In ']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Users']/../../../..")).click();
 	}
 }
